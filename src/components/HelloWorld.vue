61cabea353618c1668f80afe1f0aa0d5ed03df59
shnsx <shahin.siavash@gmail.com>
2024-05-26 22:09:03 +0330

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -19,12 +19,7 @@ const count = ref(0)
     </p>
   </div>
 
- <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >Test</a
-    >, the official Vue + Vite starter
-  </p>
+
 
 
 
