f26cc0e24cf89e6edb655bb2b37ef8f999e92c7e
shnsx <shahin.siavash@gmail.com>
2024-05-26 22:28:43 +0330

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue....'
+import { ref } from 'vue'
 
 defineProps({
   msg: String,
@@ -28,7 +28,7 @@ const count = ref(0)
   <p>
     Check out
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
+      >create-vue.................</a
     >, the official Vue + Vite starter
   </p>
   <p>
